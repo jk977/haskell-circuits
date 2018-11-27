7ad59b088afef88610dcaa966685d9f4ffc7fded
jk977 <jdk64@comcast.net>
2018-11-27 00:09:35 -0800

    Making proc-less arrow adder work (with errors)

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,18 @@ module Main where
 import Control.Arrow
 import Data.Bits
 
-adder :: (Arrow a, Bits b) => a (b,b,b) (b,b)
-adder cin = proc (x,y,cin) -> do
+-- adder implemented as a normal uncurried function
+adder :: Bits b => (b,b,b) -> (b,b)
+adder (x,y,cin) = (sumBit,carry) where
+    x1 = xor x y
+    sumBit = xor x1 cin
+    a1 = x1.&.cin
+    a2 = x.&.y
+    carry = a1.|.a2
+
+-- adder in arrow notation using proc syntax
+adder' :: (Arrow a, Bits b) => a (b,b,b) (b,b)
+adder' = proc (x,y,cin) -> do
     x1 <- xor' -< (x, y)
     sumBit <- xor' -< (x1, cin)
     a1 <- and' -< (x1, cin)
@@ -17,14 +27,9 @@ adder cin = proc (x,y,cin) -> do
         arrowize = arr . uncurry
         [xor', and', or'] = arrowize <$> [xor, (.&.), (.|.)]
 
-adder' :: Bits b => (b,b,b) -> (b,b)
-adder' (x,y,cin) = (sumBit,carry) where
-    x1 = xor x y
-    sumBit = xor x1 cin
-    a1 = x1.&.cin
-    a2 = x.&.y
-    carry = a1.|.a2
-
+-- adder in arrow notation without proc syntax
+-- the following diagram may help to understand how it's modeled:
+--
 -- cin     x      y
 --  |      |\     |  
 --  |      | \   / \  
@@ -41,15 +46,21 @@ adder' (x,y,cin) = (sumBit,carry) where
 --          |       v v
 --          v      (.|.)
 --       sumBit
+
+adder'' :: (Arrow a, Bits b) => a (b,b,b) (b,b)
 adder'' =
-    arr group
-    >>> first (arr (uncurry xor) &&& arr (uncurry (.&.)))
-    >>> first (first (arr (xor cin) &&& arr (.&.cin)))
-    >>> first (first (second . arr . uncurry $ (.|.)))
-    >>> first (second (arr const) *** first (second $ arr const))
-    >>> arr (uncurry (.|.))
+    arr (\(x,y,z) -> ((x,y),z)) &&& arr (\(x,y,_) -> (x,y)) -- (((x,y),cin), (x,y))
+    >>> second andA                                         -- (((x,y),cin), a2)
+    >>> first (first (xorA &&& andA))                       -- (((x1,a1),cin), a2)
+    >>> first (arr (\((x,y),z) -> ((x,z),y)))               -- (((x1,cin),a1), a2)
+    >>> first (first xorA)                                  -- ((sumBit,a1), a2)
+    >>> arr (\((x,y),z) -> (x,(y,z)))                       -- (sumBit,(a1,a2))
+    >>> second orA                                          -- (sumBit,carry)
     where
-        group (x,y,z) = ((x,y),z)
+        arrowize = arr . uncurry
+        andA = arrowize (.&.)
+        orA = arrowize (.|.)
+        xorA = arrowize xor
 
 main :: IO ()
 main = undefined
